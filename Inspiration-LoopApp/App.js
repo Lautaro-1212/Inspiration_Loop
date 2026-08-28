c73ec0e76ab4bb6e1eadf6bb43a055be8ce1e6ec
Lauty <Lautaro Gallo>
2026-08-28 20:52:36 -0300

    feat: agregar base de navigation

--- Inspiration-LoopApp/App.js
+++ Inspiration-LoopApp/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./screens/Inicio";
+import CrearScreen from "./screens/Crear";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Inicio"
+                    component={HomeScreen}
+                />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                <Tab.Screen
+                    name="Crear"
+                    component={CrearScreen}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
