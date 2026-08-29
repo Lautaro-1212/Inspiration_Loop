d947e040462a5bf0d0c5a2708fb55118534bd309
Lauty <Lautaro Gallo>
2026-08-28 22:28:49 -0300

    feat: agregar iconos a los tabs

--- Inspiration-LoopApp/App.js
+++ Inspiration-LoopApp/App.js
@@ -1,5 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Image } from "react-native-web";
 
 import HomeScreen from "./screens/Inicio";
 import CrearScreen from "./screens/Crear";
@@ -11,28 +12,99 @@ const Tab = createBottomTabNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={{
+                    headerShown: false,
+
+                    tabBarStyle: {
+                        position: "absolute",
+
+                        bottom: 10,
+                        left: 10,
+                        right: 10,
+                        height: 52,
+
+                        borderRadius: 20,
+
+                        backgroundColor: "#ffffff",
+
+                        //borderTopWidth: 0,
+
+                        borderWidth: 2,
+                        borderColor:'black',
+
+                        elevation: 5,
+                    },
+
+                    tabBarItemStyle: {
+                        paddingVertical: 0,
+                    },
+                }}
+            >
                 <Tab.Screen
                     name="Inicio"
                     component={HomeScreen}
+                    options={{
+                        tabBarIcon: () => (
+                            <Image
+                                source={require("./assets/Home.png")}
+                                style={{
+                                width: 30,
+                                height: 30,
+                                }}
+                            />
+                        )
+                    }}
                 />
 
                 <Tab.Screen
                     name="Crear"
                     component={CrearScreen}
+                    options={{
+                        tabBarIcon: () => (
+                            <Image
+                                source={require("./assets/Crear.png")}
+                                style={{
+                                width: 30,
+                                height: 30,
+                                }}
+                            />
+                        )
+                    }}
+                />
+
+                 <Tab.Screen
+                    name="Buscar"
+                    component={BuscarScreen}
+                    options={{
+                        tabBarIcon: () => (
+                            <Image
+                                source={require("./assets/Buscar.png")}
+                                style={{
+                                width: 32,
+                                height: 32,
+                                }}
+                            />
+                        )
+                    }}
                 />
 
                 <Tab.Screen
                     name="Perfil"
                     component={PerfilScreen}
-                />
-
-                <Tab.Screen
-                    name="Buscar"
-                    component={BuscarScreen}
+                    options={{
+                        tabBarIcon: () => (
+                            <Image
+                                source={require("./assets/Perfil.png")}
+                                style={{
+                                width: 24,
+                                height: 24, 
+                                }}
+                            />
+                        )
+                    }}
                 />
             </Tab.Navigator>
         </NavigationContainer>
-        
     );
 }
