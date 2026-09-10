dc74cb82d9c67c21242897db793442602938afe4
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-09-10 20:32:07 -0300

    feat: agregar punto de arrnacar en el backend

---
+++ backend/database/schema.js
@@ -0,0 +1,34 @@
+import db from  '../config/database.js'
+
+export function arrancarBDD(){
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS image(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            path TEXT NOT NULL,
+            with INTEGER, 
+            height INTEGER 
+        );
+
+        CREATE TABLE IF NOT EXISTS category(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL  
+        );
+
+        CREATE TABLE IF NOT EXISTS image_category(
+            id INTEGER PRIMARY KEY,
+            image_id INTEGER NOT NULL,
+            category_id INTEGER NOT NULL,
+
+            FOREIGN KEY (image_id)
+                REFERENCES image(id)
+                ON DELETE CASCADE,
+
+            FOREIGN KEY (category_id)
+                REFERENCES category(id)
+                ON DELETE CASCADE
+        );
+    `)
+
+    console.log("Se creo correctamente la BDD");
+}
