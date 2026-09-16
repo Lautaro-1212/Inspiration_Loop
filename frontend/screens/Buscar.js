fe9c01083f8d94b6b149ba6a5ab79b75ee978d06
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-09-16 20:39:30 -0300

    cambiar nombre a la carpeta de front

---
+++ frontend/screens/Buscar.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#969aa8'}}>
+            <Text style={{fontSize:20}}>Buscar</Text>
+        </View>
+    );
+}
