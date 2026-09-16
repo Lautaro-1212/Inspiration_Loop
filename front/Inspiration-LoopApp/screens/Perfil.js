fea2c41724d722ca4b6dc83c2e6524deb507173f
Lautaro-1212 <lautarojoagallo.07@gmail.com>
2026-09-16 20:53:22 -0300

    Delete front directory

--- front/Inspiration-LoopApp/screens/Perfil.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function HomeScreen() {
-    return (
-        <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#969aa8'}}>
-            <Text style={{fontSize:20}}>Perfil</Text>
-        </View>
-    );
-}
