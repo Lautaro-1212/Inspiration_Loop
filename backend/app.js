b7e88aa19539467627499e877367a79b65ea8d03
Lautaro Gallo <lautarojoagallo.07@gmail.com>
2026-09-20 16:38:08 -0300

    fix: arrelgar como se suben los paths

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,21 @@ import { startBDD } from './database/schema.js';
 import express from 'express'
 import cors from "cors";
 import imageRouter from "./controller/imageController.js"
+import path from 'path'
+import { dirname } from './config/fileStorage.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static(path.join(dirname, '..', 'uploads')));
 
 app.use("/api/images", imageRouter)
 
 function startHTTP(){
     app.listen(PORT, () => {
-        console.log(`Server start in http://localhost:${PORT}`);
+        console.log("Server start in http://localhost:${PORT}");
     });
 }
 
--- backend/config/fileStorage.js
+++ backend/config/fileStorage.js
@@ -2,12 +2,12 @@ import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+export const dirname = path.dirname(filename);
 
 export const ensureUploadDir = () => {
     try{
-        const uploadDir = path.join(__dirname, "..", "uploads", "images");
+        const uploadDir = path.join(dirname, "..", "uploads", "images");
         fs.mkdirSync(uploadDir, { recursive: true });
         console.log("Path created:", uploadDir)
 
