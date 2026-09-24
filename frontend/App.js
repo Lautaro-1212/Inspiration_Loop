ad13bdb5402c24eff9fd92a99ca9fa2e4f70f2c5
Marcos Mendy <marcosmendy2008@gmail.com>
2026-09-24 20:07:06 -0300

    feat:agregar cambios de logo por pantalla

--- frontend/App.js
+++ frontend/App.js
@@ -2,18 +2,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Image, StatusBar } from "react-native";
 
-
 import HomeScreen from "./screens/Inicio";
 import CrearScreen from "./screens/Crear";
 import PerfilScreen from "./screens/Perfil";
 import BuscarScreen from "./screens/Buscar";
 
 const Tab = createBottomTabNavigator();
-        
+
 export default function App() {
   return (
     <>
-      {/* Transparencia total en la barra superior del sistema */}
       <StatusBar
         translucent
         backgroundColor="transparent"
@@ -25,17 +23,16 @@ export default function App() {
           screenOptions={{
             headerShown: false,
             tabBarShowLabel: false,
-
             tabBarStyle: {
               position: "absolute",
               bottom: 0,
               left: 0,
               right: 0,
               height: 75,
-              backgroundColor: "#224484", // Mismo fondo gris de tus pantallas
+              backgroundColor: "#224484",
               borderTopWidth: 0,
-              elevation: 0, // Quita la línea superior y sombra en Android
-              paddingBottom: 15, // Eleva los íconos por encima de los botones de Android
+              elevation: 0,
+              paddingBottom: 15,
             },
             tabBarItemStyle: {
               justifyContent: "center",
@@ -47,64 +44,71 @@ export default function App() {
             name="Inicio"
             component={HomeScreen}
             options={{
-              tabBarIcon: () => (
+              tabBarIcon: ({ focused }) => (
                 <Image
-                  source={require("./assets/Home.png")}
-                  style={{ width: 28, height: 28, tintColor: "#000000" }}
+                  source={
+                    focused
+                      ? require("./assets/Home2.png")
+                      : require("./assets/Home.png")
+                  }
+                  style={{ width: 30, height: 30 }}
                 />
               ),
             }}
           />
 
-                <Tab.Screen
-                    name="Crear"
-                    component={CrearScreen}
-                    options={{
-                        tabBarIcon: () => (
-                            <Image
-                                source={require("./assets/Crear.png")}
-                                style={{
-                                width: 30,
-                                height: 30,
-                                }}
-                            />
-                        )
-                    }}
+          <Tab.Screen
+            name="Crear"
+            component={CrearScreen}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={
+                    focused
+                      ? require("./assets/Crear2.png")
+                      : require("./assets/Crear.png")
+                  }
+                  style={{ width: 30, height: 30 }}
                 />
+              ),
+            }}
+          />
 
-                 <Tab.Screen
-                    name="Buscar"
-                    component={BuscarScreen}
-                    options={{
-                        tabBarIcon: () => (
-                            <Image
-                                source={require("./assets/Buscar.png")}
-                                style={{
-                                width: 32,
-                                height: 32,
-                                }}
-                            />
-                        )
-                    }}
+          <Tab.Screen
+            name="Buscar"
+            component={BuscarScreen}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={
+                    focused
+                      ? require("./assets/Buscar2.png")
+                      : require("./assets/Buscar.png")
+                  }
+                  style={{ width: 32, height: 32 }}
                 />
+              ),
+            }}
+          />
 
-                <Tab.Screen
-                    name="Perfil"
-                    component={PerfilScreen}
-                    options={{
-                        tabBarIcon: () => (
-                            <Image
-                                source={require("./assets/Perfil.png")}
-                                style={{
-                                width: 24,
-                                height: 24, 
-                                }}
-                            />
-                        )
-                    }}
+          <Tab.Screen
+            name="Perfil"
+            component={PerfilScreen}
+            options={{
+              tabBarIcon: ({ focused }) => (
+                <Image
+                  source={
+                    focused
+                      ? require("./assets/Perfil2.png")
+                      : require("./assets/Perfil.png")
+                  }
+                  style={{ width: 24, height: 24 }}
                 />
-            </Tab.Navigator>
-        </NavigationContainer>
-        </>
-    );
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
